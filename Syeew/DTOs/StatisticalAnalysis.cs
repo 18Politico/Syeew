d8da6d484451d0bf1649fe81d077e1b1b8fb9764
HTTPiego <belardinelli.diego@e-lios.eu>
2022-12-15 22:02:43 +0100

    endpoints charts

---
+++ Syeew/DTOs/StatisticalAnalysis.cs
@@ -0,0 +1,30 @@
+﻿namespace Syeew.DTOs
+{
+    public class StatisticalAnalysis
+    {
+        public string CompanyName { get; set; }
+
+        //public DateTime Date { get; set; }
+
+        public int Day { get; set; }
+        public int Mouth { get; set; }
+        public int Year { get; set; }
+        public double Value { get; set; }
+
+        //public StatisticalAnalysis(string companyName, DateTime date, double value)
+        //{
+        //    CompanyName = companyName;
+        //    Date = date;
+        //    Value = value;
+        //}
+
+        public StatisticalAnalysis(string companyName, int day, int mouth, int year, double value)
+        {
+            CompanyName = companyName;
+            Day = day;
+            Mouth = mouth;
+            Year = year;
+            Value = value;
+        }
+    }
+}
