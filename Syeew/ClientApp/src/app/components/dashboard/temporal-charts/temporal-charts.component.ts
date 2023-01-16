2b29a516262110ae87a4f606c5efa1325c073d4b
Piero <piero.hierro1@gmail.com>
2023-01-16 10:58:15 +0100

    Update temporal-charts.component.ts

--- Syeew/ClientApp/src/app/components/dashboard/temporal-charts/temporal-charts.component.ts
+++ Syeew/ClientApp/src/app/components/dashboard/temporal-charts/temporal-charts.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { ThemePalette } from '@angular/material/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { BoxPlotComponent } from '../../charts/box-plot/box-plot.component';
+import { Observable } from 'rxjs';
+import { BarChartComponent } from '../../charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from '../../charts/line-chart/line-chart.component';
+import { PieChartComponent } from '../../charts/pie-chart/pie-chart.component';
+import { ScatterPlotComponent } from '../../charts/scatter-plot/scatter-plot.component';
+import { ColumnChartComponent } from '../../charts/column-chart/column-chart.component';
+import { ICompany } from 'src/app/models/interfaces/ICompany';
+import { IQuantitativeData } from 'src/app/models/interfaces/IQuantitativeData';
+import { QuantitativeData } from 'src/app/models/QuantitativeData';
 
 @Component({
   selector: 'app-temporal-charts',
   templateUrl: './temporal-charts.component.html',
   styleUrls: ['./temporal-charts.component.css']
 })
-export class TemporalChartsComponent {
+export class TemporalChartsComponent implements OnInit{
+  @ViewChild('xAxis') xAxisChoises!: any
+  
+  xAxisChoice!: string;
+
+  selectedColor: ThemePalette = 'primary'
+  choises: string[] = ['Line Chart', 'Bar Chart', 'Summer', 'Autumn'];
+  cards!: Observable<any>
+  @Input() selectedCompany!: ICompany
+  @Input() dateFrom!: string
+  @Input() dateTo?: string
+  dateFromProva = new Date(this.dateFrom)
+  @Input() filtered!: IQuantitativeData[]
+  isRefreshNeeded!: boolean
+
+  constructor(private breakpointObserver: BreakpointObserver, private dialog: MatDialog) {
+    this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+      map(({ matches }) => {
+        return [
+          { nameChart: 'Box Plot', cols: 1, rows: 1 },
+          { nameChart: 'Bar Chart', cols: 1, rows: 1 },
+          { nameChart: 'Line Chart', cols: 1, rows: 1 },
+          { nameChart: 'Column Chart', cols: 1, rows: 1 },
+          { nameChart: 'Pie Chart', cols: 1, rows: 1 },
+          { nameChart: 'Scatter Plot', cols: 1, rows: 1 },
+        ];
+      })
+    );
+
+    this.xAxisChoice = ""
+    var properties = Object.keys(new QuantitativeData())
+    this.choises = properties.slice(1, properties.length -1)
+    this.isRefreshNeeded = false
+  }
+
+  ngOnInit(): void {
+  }
+
+  openChart(chartName: string) {
+    let dialogRef: MatDialogRef<any, any>
+    switch (chartName) {
+      case 'Box Plot': {
+        dialogRef = this.dialog.open(BoxPlotComponent);
+        break;
+      }
+      case 'Bar Chart': {
+        dialogRef = this.dialog.open(BarChartComponent);
+        break;
+      }
+      case 'Line Chart': {
+        dialogRef = this.dialog.open(LineChartComponent);
+        break;
+      }
+      case 'Column Chart': {
+        dialogRef = this.dialog.open(ColumnChartComponent);
+        break;
+      }
+      case 'Pie Chart': {
+        dialogRef = this.dialog.open(PieChartComponent);
+        break;
+      }
+      case 'Box Plot Netto': {
+        dialogRef = this.dialog.open(BoxPlotComponent);
+        break;
+      }
+      case 'Scatter Plot': {
+        dialogRef = this.dialog.open(ScatterPlotComponent);
+        break;
+      }
+    }
+    dialogRef!.updateSize('300vw')
+  }
+
+  refreshChart(){
+    this.isRefreshNeeded = true
+  }
 
 }
