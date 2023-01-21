1661934c6a96b1b96e077e6f4c23c3ceb8b79413
HTTPiego <belardinelli.diego@e-lios.eu>
2023-01-21 13:25:44 +0100

    groups

--- Syeew/Controllers/PlotsController.cs
+++ Syeew/Controllers/PlotsController.cs
@@ -11,6 +11,7 @@ using Syeew.DTOs;
 using Syeew.Utils;
 using Syeew.Utils.DTOs;
 using System.Data;
+using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace Syeew.Controllers
@@ -41,7 +42,7 @@ namespace Syeew.Controllers
 
                 var filteredData = await this.GetFilteredData(request);
 
-                var grouped = filteredData.GroupBy(d => new CustomDate(d.Dt.Day, d.Dt.Month + 1, d.Dt.Year));
+                var grouped = filteredData.GroupBy(d => new {day = d.Dt.Day, month = d.Dt.Month, year = d.Dt.Year } ); //new CustomDate(d.Dt.Day, d.Dt.Month + 1, d.Dt.Year));
 
                 List<BoxPlotDataDTO> response = ObtainStatsFromGroup(grouped, request.Content);
 
@@ -73,26 +74,36 @@ namespace Syeew.Controllers
 
         private bool DateIsBetween(CustomDate customFrom, CustomDate customTo, DateTime dateToCheck)
         {
-            DateTime from = new DateTime(customFrom.Year, customFrom.Month+1, customFrom.Day);
-            DateTime to = new DateTime(customTo.Year, customTo.Month+1, customTo.Day);
+            DateTime from = new DateTime(customFrom.Year, customFrom.Month, customFrom.Day);
+            DateTime to = new DateTime(customTo.Year, customTo.Month, customTo.Day);
             return DateTime.Compare(dateToCheck, from) >= 0 && DateTime.Compare(dateToCheck, to) <= 0;
         }
 
-        private List<BoxPlotDataDTO> ObtainStatsFromGroup(IEnumerable<IGrouping<CustomDate, IQuantitativeData>> groups, string content)
+        //Object keyToObj = group.Key;
+        //int dayrrr = Int32.Parse(keyToObj.GetType().GetProperty("day")?.GetValue(keyToObj, null)?.ToString() !);
+        //int month = Int32.Parse(keyToObj.GetType().GetProperty("month")?.GetValue(keyToObj, null)?.ToString()!);
+        //int year = Int32.Parse(keyToObj.GetType().GetProperty("year")?.GetValue(keyToObj, null)?.ToString()!);
+        //CustomDate date;
+        //PropertyInfo day = keyToObj.GetType().GetProperty("day");
+
+        private List<BoxPlotDataDTO> ObtainStatsFromGroup(IEnumerable<IGrouping<dynamic, IQuantitativeData>> groups, string content)
         {
             List<BoxPlotDataDTO> statsFromGroup = new List<BoxPlotDataDTO>();
 
-            foreach (var group in groups)
+            for(int i = 0; i < groups.Count(); i++)
+            //foreach (var group in groups)
             {
-                group.Key.Month -= 1; 
-                var boxPlotData = new BoxPlotDataDTO(group.Key);
+                var group = groups.ElementAt(i);
+                var firstData = group.ElementAt(0);
+                var customDate = new CustomDate(firstData.Dt.Day, firstData.Dt.Month - 1, firstData.Dt.Year);
+                var boxPlotData = new BoxPlotDataDTO(customDate);
                 var properties = new double[group.Count()];
 
-                for (int i = 0; i < group.Count(); i++)
+                for (int j = 0; j < group.Count(); j++)
                 {
-                    var data = group.ElementAt(i);
+                    var data = group.ElementAt(j);
                     var property = data.GetType().GetProperty(content)?.GetValue(data, null)?.ToString();
-                    properties[i] = Double.Parse(property!);
+                    properties[j] = Double.Parse(property!);
                 }
                 //var Stats = new DescriptiveStatistics(properties);
                 var minimum = Statistics.Minimum(properties);
@@ -122,7 +133,7 @@ namespace Syeew.Controllers
 
                 var filteredData = await this.GetFilteredData(request);
 
-                var grouped = filteredData.GroupBy(d => new CustomDate(-1, d.Dt.Month+1, d.Dt.Year));
+                var grouped = filteredData.GroupBy(d => new {month = d.Dt.Month, year = d.Dt.Year });//new CustomDate(-1, d.Dt.Month, d.Dt.Year));
 
                 List<BoxPlotDataDTO> response = ObtainStatsFromGroup(grouped, request.Content);
 
@@ -155,7 +166,7 @@ namespace Syeew.Controllers
 
                 foreach(var data in filteredData)
                 {
-                    response.AddLast(new TemporalDataDTO(new CustomDate(data.Dt.Day, data.Dt.Month+1, data.Dt.Year),
+                    response.AddLast(new TemporalDataDTO(new CustomDate(data.Dt.Day, data.Dt.Month-1, data.Dt.Year),
                                                         Double.Parse(data.GetType().GetProperty(request.Content)?.GetValue(data, null)?.ToString()!)));
                 }
 
