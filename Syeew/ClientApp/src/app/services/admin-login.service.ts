8f93e79f5038e3086c7fc8ccf20f2d6cc71fbc79
xPieroxk <piero.hierro1@gmail.com>
2023-02-02 17:39:17 +0100

    added login and registering

--- Syeew/ClientApp/src/app/services/admin-login.service.ts
+++ Syeew/ClientApp/src/app/services/admin-login.service.ts
@@ -1,28 +1,54 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtDTO } from '../Utils/DTOs/JwtDTO'
+import { RegisterDTO } from '../Utils/DTOs/RegisterDTO'
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginDTO } from '../Utils/DTOs/LoginDTO';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminLoginService implements CanActivate {
 
-  private isAuthenticated: boolean
+  private readonly _url = "https://localhost:7290/api/"
 
-  constructor(public router: Router) {
-    this.isAuthenticated = false
-  }
+  constructor(public router: Router, private _http: HttpClient) { }
 
-  public login(username: string, password: string) {
-
+  login(username: string, password: string): Observable<JwtDTO> {
+    let user: LoginDTO = { name: username, password: password }
+    return this._http.post<JwtDTO>(this._url + 'AdminLogin', user)
   }
 
   canActivate(): boolean {
-    if (!this.isAuthenticated) {
+    if (!this.isAuthenticated()) {
       this.router.navigate(['/login'])
       return false
     }
     return true
   }
 
+  isAuthenticated(): boolean {
+    return localStorage.getItem('token') != null
+  }
+
+  getToken(): string {
+    return localStorage.getItem('token') as string
+  }
+
+  logout(): void {
+    localStorage.clear()
+  }
+
+  register(name: string, password: string, email: string, surname: string): Observable<any> {
+    let registerDto: RegisterDTO = { name: name, password: password, email: email, surname: surname }
+    return this._http.post<any>(this._url + 'Admin/RegisterNewAdmin', registerDto)
+  }
+
+  getRequiredHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.getToken()
+    });
+  }
 }
